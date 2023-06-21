f02495d62a180c4a0f03aae3b98c09e4ee4fab38
Jayan Ratna <jayanratna@gmail.com>
2023-06-21 16:57:42 +1200

    Update `from` parameter

--- index.rb
+++ index.rb
@@ -3,7 +3,7 @@ require "resend"
 Resend.api_key = ENV["RESEND_API_KEY"]
 
 params = {
-  "from": "onboarding@resend.dev",
+  "from": "Acme <onboarding@resend.dev>",
   "to": ["delivered@resend.dev"],
   "subject": "Hello World",
   "html": "<strong>It works!</strong>"
